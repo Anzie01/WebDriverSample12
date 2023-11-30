b0eb9fd4d2f59f9b7a46aba194a09d56357605c3
Anzie01 <ankita.khare89@gmail.com>
2023-11-30 07:18:43 +0000

    Changed and updated the code

--- src/test/java/testScript/LocatorTest.java
+++ src/test/java/testScript/LocatorTest.java
@@ -3,14 +3,13 @@ package testScript;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GooglePageTest {
+public class LocatorTest {
 
 	public static void main(String[] args) {
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
 		
-WebDriver driver= new ChromeDriver();
-driver.manage().window().maximize();
-driver.get("google.com");
+
 	}
-	
 
 }
