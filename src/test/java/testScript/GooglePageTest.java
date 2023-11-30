e2ee5491a25d324bbb55fd8b2d8946ae9948008e
Anzie01 <ankita.khare89@gmail.com>
2023-11-30 06:46:03 +0000

    Initail Project with WebDriverSample

---
+++ src/test/java/testScript/GooglePageTest.java
@@ -0,0 +1,15 @@
+package testScript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GooglePageTest {
+
+	public static void main(String[] args) {
+		
+WebDriver driver= new ChromeDriver();
+driver.manage().window().maximize();
+	}
+	
+
+}
