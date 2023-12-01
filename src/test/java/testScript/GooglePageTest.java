954b36f7af839269cdec73a8e29517f2ecbbd7c5
Administrator <Administrator@172.31.2.90>
2023-12-01 06:45:44 +0000

    Updated the code

--- src/test/java/testScript/GooglePageTest.java
+++ src/test/java/testScript/GooglePageTest.java
@@ -2,14 +2,22 @@ package testScript;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GooglePageTest {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		ChromeOptions options= new ChromeOptions();
+		options.setBrowserVersion("115");
 		
-WebDriver driver= new ChromeDriver();
+		
+WebDriver driver= new ChromeDriver(options);
 driver.manage().window().maximize();
-driver.get("google.com");
+driver.manage().deleteAllCookies();
+driver.get("https://www.google.com/");
 	}
 	
 
--- src/test/java/testScript/LocatorTest.java
+++ src/test/java/testScript/LocatorTest.java
@@ -1,5 +1,6 @@
 package testScript;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,8 +9,8 @@ public class LocatorTest {
 	public static void main(String[] args) {
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
-		
-
+		driver.get("https://www.selenium.dev/");
+		driver.findElement(By.cssSelector(""));
 	}
 
 }
